8ef778b63cac8c0014b61b77bcd5c552d986872a
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2018-07-07 19:57:36 +0900

    changed cache target to cdn

--- seireki_wareki/build.sh
+++ seireki_wareki/build.sh
@@ -14,9 +14,6 @@ cp -a src/images dist/ 2>/dev/null
 cp -a src/css dist/ 2>/dev/null
 
 mkdir dist/js
-cp src/js/bootstrap-material-design.min.js dist/js/ 2>/dev/null
-cp src/js/jquery-3.3.1.slim.min.js dist/js/ 2>/dev/null
-cp src/js/popper.min.js dist/js/ 2>/dev/null
 
 webpack
 
