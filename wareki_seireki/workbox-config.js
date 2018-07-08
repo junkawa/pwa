7bf77ebda4fee42270f3331196b9c14bd595ed88
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2018-07-08 16:24:25 +0900

    Change cache target from static files to cdn fetch

--- wareki_seireki/workbox-config.js
+++ wareki_seireki/workbox-config.js
@@ -3,5 +3,9 @@ module.exports = {
   "globPatterns": [
     "**/*.{css,png,html,js,json}"
   ],
-  "swDest": "dist/sw.js"
+  "swDest": "dist/sw.js",
+  runtimeCaching: [
+    {urlPattern: /^https:\/\/unpkg\.com/, handler: 'cacheFirst'},
+    {urlPattern: /^https:\/\/code\.jquery\.com/, handler: 'cacheFirst'},
+  ]
 };
