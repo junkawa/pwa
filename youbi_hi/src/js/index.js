fee9fed9cad94973f558d35fe19a5a7a2257aac7
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2018-07-09 15:49:23 +0900

    Add button click eventlisteners

--- youbi_hi/src/js/index.js
+++ youbi_hi/src/js/index.js
@@ -10,3 +10,47 @@ var app = require('./app');
     }
 })();
 
+$('#tensplaceList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setTensplace($(this).attr('id'));
+
+    updateView();
+})
+
+$('#onesplaceList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setOnesplace($(this).attr('id'));
+
+    updateView();
+})
+
+$('#monthList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setMonth($(this).attr('id'));
+
+    updateView();
+})
+
+$('#weekList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setWeek($(this).attr('id'));
+
+    updateView();
+})
+
+const updateView = () => {
+    updateView1();
+    updateView2();
+};
+
+const updateView1 = () => {
+    $('#view1').text(app.getTextView1());
+};
+
+const updateView2 = () => {
+    $('#view2').text(app.getTextView2());
+};
