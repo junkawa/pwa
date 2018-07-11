bd1cd2f462a4aba8bb72cecfadea6320331300fc
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2018-07-11 18:05:02 +0900

    Add button click events

--- yearweek_day/src/js/app.js
+++ yearweek_day/src/js/app.js
@@ -1,2 +1,128 @@
 'use strict';
 
+let yearHundredsPlace = '20';
+let yearTensPlace = '1';
+let yearOnesPlace = '8';
+let month = '7';
+let weekTensPlace = '1';
+let weekOnesPlace = '0';
+
+const setYearHundredsPlace = (h) => {
+    switch(h) {
+    case '19':
+    case '20':
+	yearHundredsPlace = h;
+	break;
+    default:
+	throw new Error('invalid argument:'+h);
+    }
+};
+
+const setYearTensPlace = (t) => {
+    switch(t) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+	yearTensPlace = t;
+	break;
+    default:
+	throw new Error('invalid argument:'+t);
+    }
+};
+
+const setYearOnesPlace = (o) => {
+    switch(o) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+	yearOnesPlace = o;
+	break;
+    default:
+	throw new Error('invalid argument:'+o);
+    }
+};
+
+const setMonth = (m) => {
+    switch(m) {
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '10':
+    case '11':
+    case '12':
+	month = m;
+	break;
+    default:
+	throw new Error('invalid argument:'+m);
+    }
+};
+
+const setWeekTensPlace = (t) => {
+    switch(t) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+	weekTensPlace = t;
+	break;
+    default:
+	throw new Error('invalid argument:'+t);
+    }
+};
+
+const setWeekOnesPlace = (o) => {
+    switch(o) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+	weekOnesPlace = o;
+	break;
+    default:
+	throw new Error('invalid argument:'+o);
+    }
+};
+
+const getTextView1_ = (yh, yt, yo, m, wt, wo) => {
+    return '';
+};
+
+const getTextView1 = () => {
+    return getTextView1_(yearHundredsPlace, yearTensPlace, yearOnesPlace, month, weekTensPlace, weekOnesPlace);
+};
+
+const getTextView2_ = (yh, yt, yo, m, wt, wo) => {
+    return '';
+};
+
+const getTextView2 = () => {
+    return getTextView2_(yearHundredsPlace, yearTensPlace, yearOnesPlace, month, weekTensPlace, weekOnesPlace);
+};
+
+module.exports = {setYearHundredsPlace, setYearTensPlace, setYearOnesPlace, setMonth, setWeekTensPlace, setWeekOnesPlace, getTextView1, getTextView2};
--- yearweek_day/src/js/index.js
+++ yearweek_day/src/js/index.js
@@ -2,10 +2,62 @@
 
 var app = require('./app');
 
+const updateView = () => {
+    $('#view1').text(app.getTextView1());
+    $('#view2').text(app.getTextView2());
+};
+
+$('#yearHundredsPlaceList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setYearHundredsPlace($(this).attr('id'));
+
+    updateView();
+});
+
+$('#yearTensPlaceList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setYearTensPlace($(this).attr('id'));
+
+    updateView();
+});
+
+$('#yearOnesPlaceList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setYearOnesPlace($(this).attr('id'));
+
+    updateView();
+});
+
+$('#monthList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setMonth($(this).attr('id'));
+
+    updateView();
+});
+
+$('#weekTensPlaceList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setWeekTensPlace($(this).attr('id'));
+
+    updateView();
+});
+
+$('#weekOnesPlaceList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setWeekOnesPlace($(this).attr('id'));
+
+    updateView();
+});
+
 (function() {
     if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
-            .register('./sw.js')
-            .then(function() { console.log('Service Worker Registered'); });
+            .register('./sw.js');
     }
 })();
