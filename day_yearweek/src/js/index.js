9d87dc98372cd354734e149a8a6549743bb6bc4a
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2018-07-12 09:29:01 +0900

    Add button click events

--- day_yearweek/src/js/index.js
+++ day_yearweek/src/js/index.js
@@ -2,10 +2,62 @@
 
 var app = require('./app');
 
+const updateView = () => {
+    $('#view1').text(app.getTextView1());
+    $('#view2').text(app.getTextView2());
+};
+
+$('#yearHundredsPlaceList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setYearHundredsPlace($(this).attr('id'));
+
+    updateView();
+});
+
+$('#yearTensPlaceList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setYearTensPlace($(this).attr('id'));
+
+    updateView();
+});
+
+$('#yearOnesPlaceList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setYearOnesPlace($(this).attr('id'));
+
+    updateView();
+});
+
+$('#monthList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setMonth($(this).attr('id'));
+
+    updateView();
+});
+
+$('#dayTensPlaceList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setDayTensPlace($(this).attr('id'));
+
+    updateView();
+});
+
+$('#dayOnesPlaceList a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    app.setDayOnesPlace($(this).attr('id'));
+
+    updateView();
+});
+
 (function() {
     if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
-            .register('./sw.js')
-            .then(function() { console.log('Service Worker Registered'); });
+            .register('./sw.js');
     }
 })();
