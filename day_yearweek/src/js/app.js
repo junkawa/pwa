dbf39dcafa0c37326790714546d436156445246c
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2018-07-12 11:11:08 +0900

    Add getTextView2

--- day_yearweek/src/js/app.js
+++ day_yearweek/src/js/app.js
@@ -109,24 +109,64 @@ const setDayOnesPlace = (o) => {
     }
 };
 
-const getDate_ = (yh, yt, yo, m, dt, dop) => {
-    if (dt+dop === '00') return null;
+const invalidDate_ = (yh, yt, yo, m, dt, dop) => {
+    if (dt+dop === '00') return true;
 
     {
 	const dStr = `${yh}${yt}${yo}/${m}/1`;
 	let d = new Date(dStr);
 	const lastDay = (new Date(d.getFullYear(), d.getMonth() + 1, 0)).getDate();
-	if (Number(dt+dop) > lastDay) return null;
+	if (Number(dt+dop) > lastDay) return true;
     }
 
     const dStr = `${yh}${yt}${yo}/${m}/${dt}${dop}`;
     let d = new Date(dStr);
-    if (d.toString() === 'Invalid Date') return null;
-    return d;
+    if (d.toString() === 'Invalid Date') return true;
+    
+    return false;
 };
 
+const isNextYearsWeek_ = (m, d, w) => {
+    if (!(m===12 && d >= 29)) return false;
+
+    const day = (w+6)%7; // mon:0, tue:1, ...
+
+    switch (d) {
+    case 29:
+	if (day === 0) return true; // 12/29 (mon)
+	break;
+    case 30:
+	if (day <= 1) return true; // 12/30 (mon or tue)
+	break;
+    case 31:
+	if (day <= 2) return true; // 12/31 (mon, tue, or wed)
+	break;
+    }
+    return false;
+}
+
+const getWeek_ = (y, m, d) => {
+    let last = new Date(y, m-1, d);
+
+    if (isNextYearsWeek_(m, d, last.getDay())) return 1; // 12/29,30,31 is next year's week
+    
+    // http://ltd.hatenablog.com/entry/2014/07/02/181833
+    let oneJan = new Date(y, 0, 1); // JAN/1
+    let day = Math.ceil((last - oneJan) / 86400000);
+    const offset = oneJan.getDay() - 1;
+    const week = Math.floor((day + offset) / 7) + 1;
+
+    let diff = 0;
+    if ((oneJan.getDay()+6)%7 >= 4) diff = 1; // 1/1 and 1/4 are not in the same week
+
+    if (week-diff === 0) return getWeek_(y-1,12,28); // last weeks of the last year
+ 
+
+    return week - diff;
+}
+
 const getTextView1_ = (yh, yt, yo, m, dt, dop) => {
-    if (getDate_(yh, yt, yo, m, dt, dop) === null) return '--';
+    if (invalidDate_(yh, yt, yo, m, dt, dop)) return '--';
 
     if (dt === '0') dt = '';
 
@@ -138,7 +178,10 @@ const getTextView1 = () => {
 };
 
 const getTextView2_ = (yh, yt, yo, m, dt, dop) => {
-    return '--';
+    if (invalidDate_(yh, yt, yo, m, dt, dop)) return '';
+
+    const week = getWeek_(Number(yh+yt+yo),Number(m),Number(dt+dop));
+    return `第${week}週`;
 };
 
 const getTextView2 = () => {
--- day_yearweek/test/test.js
+++ day_yearweek/test/test.js
@@ -40,4 +40,90 @@ describe('app', function () {
             });
 	}
     });
+    describe('getTextView2', function () {
+	{
+	    const expect = '第52週';
+            it('returns '+expect, function () {
+		app.setYearHundredsPlace('20');
+		app.setYearTensPlace('1');
+		app.setYearOnesPlace('1');
+		app.setMonth('1');
+		app.setDayTensPlace('0');
+		app.setDayOnesPlace('2');
+		assert.equal(app.getTextView2(), expect);
+            });
+	}
+	{
+	    const expect = '第1週';
+            it('returns '+expect, function () {
+		app.setYearHundredsPlace('20');
+		app.setYearTensPlace('1');
+		app.setYearOnesPlace('1');
+		app.setMonth('1');
+		app.setDayTensPlace('0');
+		app.setDayOnesPlace('3');
+		assert.equal(app.getTextView2(), expect);
+            });
+	}
+	{
+	    const expect = '第1週';
+            it('returns '+expect, function () {
+		app.setYearHundredsPlace('20');
+		app.setYearTensPlace('0');
+		app.setYearOnesPlace('8');
+		app.setMonth('12');
+		app.setDayTensPlace('3');
+		app.setDayOnesPlace('1');
+		assert.equal(app.getTextView2(), expect);
+            });
+	}
+	{
+	    const expect = '第53週';
+            it('returns '+expect, function () {
+		app.setYearHundredsPlace('20');
+		app.setYearTensPlace('0');
+		app.setYearOnesPlace('9');
+		app.setMonth('12');
+		app.setDayTensPlace('2');
+		app.setDayOnesPlace('8');
+		assert.equal(app.getTextView2(), expect);
+            });
+	}
+	{
+	    const expect = '第1週';
+            it('returns '+expect, function () {
+		app.setYearHundredsPlace('20');
+		app.setYearTensPlace('0');
+		app.setYearOnesPlace('8');
+		app.setMonth('12');
+		app.setDayTensPlace('2');
+		app.setDayOnesPlace('9');
+		assert.equal(app.getTextView2(), expect);
+            });
+	}
+	{
+	    const expect = '第1週';
+            it('returns '+expect, function () {
+		app.setYearHundredsPlace('20');
+		app.setYearTensPlace('0');
+		app.setYearOnesPlace('9');
+		app.setMonth('1');
+		app.setDayTensPlace('0');
+		app.setDayOnesPlace('4');
+		assert.equal(app.getTextView2(), expect);
+            });
+	}
+	{
+	    const expect = '';
+            it('returns '+expect, function () {
+		app.setYearHundredsPlace('20');
+		app.setYearTensPlace('1');
+		app.setYearOnesPlace('8');
+		app.setMonth('12');
+		app.setDayTensPlace('0');
+		app.setDayOnesPlace('0');
+		assert.equal(app.getTextView2(), expect);
+            });
+	}
+    });
 });
