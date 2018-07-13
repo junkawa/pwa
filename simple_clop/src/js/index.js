b3e7a816794ea5c6caef2e9134502199a88ecd15
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2018-07-13 16:37:30 +0900

    Add open button

--- simple_clop/src/js/index.js
+++ simple_clop/src/js/index.js
@@ -3,8 +3,38 @@
 var app = require('./app');
 var app = require('./croppie.min');
 
+const defaultSvgAttr = () => {
+    $('#outline-insert-photo')
+        .mouseover(function(e) {
+            $('#outline-insert-photo-line').attr('fill','#141414');
+        }).mouseout(function(e) {
+            $('#outline-insert-photo-line').attr('fill','#747474');
+        });
+        // .attr('width', '30vw')
+        // .attr('height', '30vw')
+};
+
 (function() {
-    $('.my-image').croppie();
+    const h = $(window).height();
+    const vw = h / 3.5; // reflect browser height for responsive design
+    const vh = vw; // 1:1
+    const bw = h / 2; // fit crop-container(width:50vh)
+    const bh = bw * 5 / 7; // 7:5
+
+    //7:5
+    $('.my-image').croppie({
+        //enableExif: true,
+        viewport: {
+            width: vw,
+            height: vh,
+//            type: 'circle'
+        },
+        boundary: {
+            width: bw,
+            height: bh
+        }
+    });
+    defaultSvgAttr();
 })();
 
 (function() {
