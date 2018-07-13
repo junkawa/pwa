408046c7fefdc29814f63cb2eef3c3ee7658e750
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2018-07-13 22:10:35 +0900

    Add save file

--- simple_clop/src/js/index.js
+++ simple_clop/src/js/index.js
@@ -3,6 +3,7 @@
 var app = require('./app');
 var croppie = require('./croppie.min');
 var uploadCrop = null;
+var uploadFileName;
 
 const getCrop = () => {
     if (uploadCrop != null) return uploadCrop;
@@ -30,11 +31,13 @@ const getCrop = () => {
     return uploadCrop;
 };
 
-const handleFiles = (file) => {
+const uploadFiles = (file) => {
     if (file) {
         let reader = new FileReader();
         
         reader.onload = function (e) {
+            // console.log(file['name']);
+            uploadFileName = file['name'];
             let crop = getCrop();
             crop.croppie('bind', {
                 url: e.target.result
@@ -43,6 +46,36 @@ const handleFiles = (file) => {
         
         reader.readAsDataURL(file);
     }
+};
+
+const download = (url, filename) => {
+    let a = document.createElement('a');
+    let e = document.createEvent('MouseEvent');
+    a.download = filename;
+    a.href = url;
+    e.initEvent("click", true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+    a.dispatchEvent(e);
+    // let a = $('<a>').attr({
+    //     href: url,
+    //     download: filename
+    // });
+    // a.trigger('click');
+};
+
+const saveFile = () => {
+    if (uploadCrop === null) return;
+    
+    const fmt = 'png'
+
+    let crop = uploadCrop;
+    crop.croppie('result', {
+        type: 'rawcanvas',
+        format: fmt
+    }).then(function (canvas) {
+        const reg=/(.*)(?:\.([^.]+$))/;
+        const filename = `${uploadFileName.match(reg)[1]}.${fmt}`;
+        download(canvas.toDataURL(), filename);
+    });
 };
 
 const defaultSvgAttr = (svgId, gId) => {
@@ -58,9 +91,9 @@ const defaultSvgAttr = (svgId, gId) => {
 };
 
 (function() {
-    // Webpack production mode changes handleFiles name in js file.
-    // handleFiles in input tag in html can't the method in js file.
-    $('#uploadFileElem').change(function() { handleFiles($(this).prop('files')[0]); });
+    // Webpack production mode changes uploadFiles name in js file.
+    // uploadFiles in input tag in html can't the method in js file.
+    $('#uploadFileElem').change(function() { uploadFiles($(this).prop('files')[0]); });
     
     defaultSvgAttr('#outline-insert-photo', '#outline-insert-photo-line');
     $('#outline-insert-photo').on({
@@ -72,6 +105,7 @@ const defaultSvgAttr = (svgId, gId) => {
     defaultSvgAttr('#outline-save-alt', '#outline-save-alt-line');
     $('#outline-save-alt').on({
         'click': function(e) {
+            saveFile();
         }
     });
 })();
