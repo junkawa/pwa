a7a8c02d8095d8291625ba493f4b90bd1addd054
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2018-07-13 18:36:15 +0900

    Add file open

--- simple_clop/src/js/index.js
+++ simple_clop/src/js/index.js
@@ -1,20 +1,12 @@
 'use strict';
 
 var app = require('./app');
-var app = require('./croppie.min');
+var croppie = require('./croppie.min');
+var uploadCrop = null;
 
-const defaultSvgAttr = () => {
-    $('#outline-insert-photo')
-        .mouseover(function(e) {
-            $('#outline-insert-photo-line').attr('fill','#141414');
-        }).mouseout(function(e) {
-            $('#outline-insert-photo-line').attr('fill','#747474');
-        });
-        // .attr('width', '30vw')
-        // .attr('height', '30vw')
-};
-
-(function() {
+const getCrop = () => {
+    if (uploadCrop != null) return uploadCrop;
+    
     const h = $(window).height();
     const vw = h / 3.5; // reflect browser height for responsive design
     const vh = vw; // 1:1
@@ -22,24 +14,63 @@ const defaultSvgAttr = () => {
     const bh = bw * 5 / 7; // 7:5
 
     //7:5
-    $('.my-image').croppie({
-        //enableExif: true,
+    uploadCrop = $('#upload-image').croppie({
+        // enableExif: true,
+        // enableResize: true,
         viewport: {
             width: vw,
             height: vh,
-//            type: 'circle'
+            // type: 'circle'
         },
         boundary: {
             width: bw,
             height: bh
         }
     });
+    return uploadCrop;
+};
+
+const handleFiles = (file) => {
+    if (file) {
+        let reader = new FileReader();
+        
+        reader.onload = function (e) {
+            let crop = getCrop();
+            crop.croppie('bind', {
+                url: e.target.result
+            });
+        }
+        
+        reader.readAsDataURL(file);
+    }
+};
+
+const defaultSvgAttr = () => {
+    $('#outline-insert-photo')
+        .on({
+            'mouseover': function(e) {
+                $('#outline-insert-photo-line').attr('fill','#141414');
+            },
+            'mouseout': function(e) {
+                $('#outline-insert-photo-line').attr('fill','#747474');
+            },
+            'click': function(e) {
+                $('#fileElem').click();
+            }
+        });
+};
+
+(function() {
     defaultSvgAttr();
+    
+    // Webpack production mode changes handleFiles name in js file.
+    // handleFiles in input tag in html can't the method in js file.
+    $('#fileElem').change(function() { handleFiles($(this).prop('files')[0]); });
 })();
 
 (function() {
     if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
+        navigator.serviceWorker
             .register('./sw.js');
     }
 })();
