6a422377639b97cbc5024e32e90a64e085ab6df1
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2018-07-14 17:40:30 +0900

    Add w strings

--- kusahayasu/src/js/index.js
+++ kusahayasu/src/js/index.js
@@ -2,6 +2,80 @@
 
 var app = require('./app');
 
+const getWStr = (str) => {
+    
+    const returnEscaped = str.replace(/\r\n/, '\n');
+    let spread = [...returnEscaped];
+    
+    let ret = '';
+    
+    for (let c of spread) {
+        if (c === '\n')
+            ret += 'ww'+c;
+        else
+            ret += c+'w';
+        // console.log(c);
+    }
+    ret += 'ww';
+    
+    const sanitizedOutput = ret.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;');
+
+    return sanitizedOutput.replace(/\n/g, '<br>');
+};
+
+const updateView = () => {
+    const input = $('#inputTextarea').val();
+    if (input.length === 0) {
+        $('#viewArea').text('');
+        return;
+    }
+
+    const output = getWStr(input);
+    
+    //$('#viewArea').text(output);
+    $('#viewArea').html(output);
+};
+
+const defaultSvgAttr = (svgId, gId) => {
+    $(svgId)
+        .on({
+            'mouseover': function() {
+                $(gId).attr('fill','#141414');
+            },
+            'mouseout': function() {
+                $(gId).attr('fill','#747474');
+            }
+        });
+};
+
+(function() {
+    defaultSvgAttr('#svg-w', '#svg-w-line');
+    $('#svg-w').on({
+        'click': function() {
+            updateView();
+        }
+    });
+    
+    defaultSvgAttr('#outline-save-alt', '#outline-save-alt-line');
+    $('#outline-save-alt').on({
+        'click': function() {
+            saveFile();
+        }
+    });
+    
+    defaultSvgAttr('#outline-open-in-new', '#outline-open-in-new-line');
+    $('#outline-open-in-new').on({
+        'click': function() {
+            let a = $('<a></a>').on({
+                'click': function() {
+                    location.href = "../";
+                }
+            });
+            a.trigger('click');
+        }
+    });
+})();
+
 (function() {
     if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
