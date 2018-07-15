5ef8001d4208149124456cb9e05254a827157fd5
Jun Kawashima <5635944+junkawa@users.noreply.github.com>
2018-07-15 18:14:00 +0900

    Add xkcd password tool

--- xkcdpass/src/js/app.js
+++ xkcdpass/src/js/app.js
@@ -1,2 +1,26 @@
 'use strict';
 
+const noun = require('./noun.json');
+
+const getHepburn = (str) => {
+    return str.replace(/si/g, 'shi').replace(/ti/g, 'chi').replace(/tu/g, 'tsu').replace(/hu/g, 'fu').replace(/zi/g, 'ji').replace(/di/g, 'ji').replace(/du/g, 'zu');
+};
+
+const getPassword = (wordNum, isHepburn) => {
+    const max = noun.length;
+    const ret = [];
+    for (let i=0; i< wordNum; i++) {
+        //const n = Math.floor(Math.random() * max);
+        const rand = window.crypto.getRandomValues(new Uint32Array(1))[0]/65536/65536;
+        // console.log(window.crypto.getRandomValues(new Uint8Array(1))[0]);
+        
+        // 0..nounJson.length-1
+        const n = Math.floor(rand * max);
+        let out = noun[n].replace(/-/g, '');
+        if (isHepburn) out = getHepburn(out);
+        ret.push(out);
+    }
+    return ret;
+};
+
+module.exports = {getPassword}
--- xkcdpass/src/js/index.js
+++ xkcdpass/src/js/index.js
@@ -1,10 +1,60 @@
 'use strict';
 
-const noun = import('./noun.json');
 const app = require('./app');
 
+const defaultSvgAttr = (svgId, gId) => {
+    $(svgId)
+        .on({
+            'mouseover': function() {
+                $(gId).attr('fill','#141414');
+            },
+            'mouseout': function() {
+                $(gId).attr('fill','#747474');
+            }
+        });
+};
 
-(function() {
+const updateView = () => {
+    const maxWordNum = 6;
+    const wordNum = $("input[name='wordNumRadios']:checked").val();
+    const isHepburn = $("#hepburn").prop("checked");
+    const passwords = app.getPassword(wordNum, isHepburn);
+    let updateNum = 0;
+    passwords.forEach( (v, i) => {
+        const id = '#word'+i;
+        $(id).text(v);
+        updateNum = i;
+    });
+    for (let i = updateNum+1; i<maxWordNum; i++) {
+        const id = '#word'+i;
+        $(id).text('');
+    }
+};
+
+(() => {
+    updateView();
+    
+    defaultSvgAttr('#svg-replay', '#svg-replay-line');
+    $('#svg-replay').on({
+        'click': function() {
+            updateView();
+        }
+    });
+    
+    defaultSvgAttr('#outline-open-in-new', '#outline-open-in-new-line');
+    $('#outline-open-in-new').on({
+        'click': function() {
+            let a = $('<a></a>').on({
+                'click': function() {
+                    location.href = "../";
+                }
+            });
+            a.trigger('click');
+        }
+    });
+})();
+
+(() => {
     if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
             .register('./sw.js');
